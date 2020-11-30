df0ccefd3c12642553c2a41f2d6555cb8c9d99eb
PCQ223 <pcq223@PCQ223.local>
2020-11-30 13:37:33 +0530

    Calendar added

--- app/screens/context-screen/context-screen.tsx
+++ app/screens/context-screen/context-screen.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react"
+import React,{useEffect,useContext} from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { Screen, Text } from "../../components"
@@ -7,14 +7,17 @@ import { Screen, Text } from "../../components"
 import { color } from "../../theme"
 import { createNativeStackNavigator } from "react-native-screens/native-stack"
 import { WelcomeScreen, DemoScreen, LoginScreen, HomeScreen } from ".."
+import { CalendarScreen } from "../calendar-screen/calendar-screen"
 //import AsyncStorage from '@react-native-async-storage/async-storage'
-
+import { NavigationContainer, NavigationContainerRef } from "@react-navigation/native"
+import { MapScreen } from "../map-screen/map-screen"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.black,
   flex: 1,
 }
 
-
+//const  AuthContext = React.createContext();
 export const ContextScreen = observer(function ContextScreen() {
   // Pull in one of our MST stores
   const { userStore} = useStores();
@@ -23,9 +26,9 @@ export const ContextScreen = observer(function ContextScreen() {
   
   // const rootStore = useStores()
   const Stack = createNativeStackNavigator()
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
-  return (
+  const Rootstack =() =>{
+    const RootStack = createNativeStackNavigator()
+    return(
     <Stack.Navigator
     //initialRouteName="login"
       screenOptions={{
@@ -33,15 +36,46 @@ export const ContextScreen = observer(function ContextScreen() {
         gestureEnabled: true,
       }}
     >
-     {userStore.name === '' ?
-        <Stack.Screen name="login" component={LoginScreen} />
-      :
-        <Stack.Screen name="home" component={HomeScreen} />
-     }
-       
-      
      
-
+        <Stack.Screen name="login" component={LoginScreen} />
+      
     </Stack.Navigator>
+    )
+  }
+  const AppStack =() =>{
+    const AppStack = createNativeStackNavigator()
+    const Tab = createBottomTabNavigator()
+    return(
+
+    <Tab.Navigator
+    initialRouteName="home"
+      screenOptions={{
+        headerShown: true,
+        gestureEnabled: true,
+        
+      }}
+    >
+     
+     <Tab.Screen name="home" component={HomeScreen} options={{headerShown : false}}/>
+      <Tab.Screen name="calendar" component={CalendarScreen} />
+      <Tab.Screen name="map" component={MapScreen} />
+      
+    </Tab.Navigator>
+    )
+  }
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+  
+   <NavigationContainer independent={true}>
+     {userStore.name === '' ?
+        <Rootstack />
+      :(
+       <AppStack />
+      )
+     }
+
+       </NavigationContainer>
+  
   )
 })
